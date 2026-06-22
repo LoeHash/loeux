61e566d8754024f298a6aed5a64cf7297757d2c5
LoeHash <2679792783@qq.com>
2026-06-22 16:27:23 +0800

    <feat> the prototype printing

--- kernel/main.c
+++ kernel/main.c
@@ -1,46 +1,18 @@
 // 内核初始化函数
 // 我们只进行一些颜色显示
+#include "fonts/font.h"
+#include "printk.h"
+
 #define COLOR_RIBBON_WEITH 25
+#define GET_BIT(val, n) (((val) >> (n)) & 1)
+
 void kernel_start(void)
 {
-        int *addr = (int *)0xffff800000a00000;
-        int i;
 
-        for (int j = 0; j < 900 / COLOR_RIBBON_WEITH; i++)
-        {
-                // 第一个色带
-                for (i = 0; i < 1440 * 20; i++)
-                {
-                        *(((char *)(addr)) + 0) = (char)0xff;
-                        *(((char *)(addr)) + 1) = (char)0x00;
-                        *(((char *)(addr)) + 2) = (char)0x00;
-                        *(((char *)(addr)) + 3) = (char)0x00;
-                        addr++;
-                }
-
-                // 第二个色带
-                for (i = 0; i < 1440 * 20; i++)
-                {
-                        *(((char *)(addr)) + 0) = (char)0x00;
-                        *(((char *)(addr)) + 1) = (char)0xff;
-                        *(((char *)(addr)) + 2) = (char)0x00;
-                        *(((char *)(addr)) + 3) = (char)0x00;
-                        addr++;
-                }
-
-                // 第三个色带
-                for (i = 0; i < 1440 * 20; i++)
-                {
-                        *(((char *)(addr)) + 0) = (char)0x00;
-                        *(((char *)(addr)) + 1) = (char)0x00;
-                        *(((char *)(addr)) + 2) = (char)0xff;
-                        *(((char *)(addr)) + 3) = (char)0x00;
-                        addr++;
-                }
-        }
-
+        init_printing();
+        color_printk("Booting The Loeux Kernel.... Please Wait...");
         while (1)
         {
-                /* code */
+                // 空循环
         }
 }
